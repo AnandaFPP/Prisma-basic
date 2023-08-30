a4df79157d880ad1092a815376e2a1903639f19a
Ananda Fadhilah <perkasa.putra1404@gmail.com>
2023-08-30 11:52:30 +0800

    CRUD implementation

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient()
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json())
+
+app.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const user = await prisma.User.findUnique({
+    where: {
+      id,
+    },
+  })
+  res.send(user)
+})
+
+app.get('/', async (req, res) => {
+  const users = await prisma.User.findMany()
+  res.send(users)
+})
+
+app.post('/', async (req, res) => {
+  const { name, email } = req.body;
+  const user = await prisma.User.create({
+    data: {
+      name,
+      email,
+    },
+  })
+  res.send("Data created successfully!")
+})
+
+app.put('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const { name, email } = req.body;
+  const user = await prisma.User.update({
+    where: {
+      id
+    },
+    data: {
+      name,
+      email,
+    },
+  })
+  res.send("Data updated successfully!")
+})
+
+app.delete('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const user = await prisma.User.delete({
+    where: {
+      id,
+    },
+  })
+  res.send("Data deleted successfully!")
 })
 
 app.listen(port, () => {
